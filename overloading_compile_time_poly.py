a193f45e3f9feebd54bb4f6083b780c2dbb7429b
john suresh <158501117+johnsureshp@users.noreply.github.com>
2024-03-16 15:52:52 +0530

    Add files via upload

---
+++ overloading_compile_time_poly.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 15 11:13:32 2024
+
+@author: P.JOHN
+"""
+
+class addition:
+    def add(self,a):
+        print(a)
+    def add(self,a,b):
+        print(a+b)
+    def add(self,a,b,c):
+        print(a+b+c)
+o=addition()
+
+#o.add(10)
+o.add(10,20)
+o.add(1,2,3)
