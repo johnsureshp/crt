18d750d4364632696d10fe1d0c7ed80f4a23aff1
john suresh <158501117+johnsureshp@users.noreply.github.com>
2024-03-16 15:54:34 +0530

    Add files via upload

---
+++ pattern1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 09:24:32 2024
+
+@author: P.JOHN
+"""
+
+for i in range(3):
+    #print()
+    for j in range(5):
+        print('*',end=" ")
+    print()
+        
